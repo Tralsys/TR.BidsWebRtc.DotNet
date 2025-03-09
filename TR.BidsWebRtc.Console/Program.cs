125f4fdd4fa82f0d8cfadbd97bda09100449e1ba
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2025-03-10 00:14:55 +0900

    ランダム書き込みのテスト用、クライアントのFirebaseからの分離に対応

--- TR.BidsWebRtc.Console/Program.cs
+++ TR.BidsWebRtc.Console/Program.cs
@@ -1,44 +1,113 @@
 using System;
+using System.IO;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
+using TR.BIDSSMemLib;
 using TR.BidsWebRtc.Api;
 using TR.BidsWebRtc.WebRtc;
 
 namespace TR.BidsWebRtc.Console;
 
-class Program
+class Program : IDisposable
 {
 	static readonly HttpClient httpClient = new();
-	const string TOKEN_REFRESH_URL = "http://127.0.0.1:9099/securetoken.googleapis.com/v1/token?key=dummy";
-	static readonly FirebaseAuthTokenManager tokenManager = new(
-		httpClient,
-		"",
-		"eyJfQXV0aEVtdWxhdG9yUmVmcmVzaFRva2VuIjoiRE8gTk9UIE1PRElGWSIsImxvY2FsSWQiOiI2V1c1N0J0UGJJcXJ3Y3ljOXRUNEdybGQyY2VEIiwicHJvdmlkZXIiOiJwYXNzd29yZCIsImV4dHJhQ2xhaW1zIjp7fSwicHJvamVjdElkIjoiYmlkcy1ydGMifQ=="
-	);
-	static readonly SdpExchangeApi sdpExchangeApi = new(
-		httpClient,
-		tokenManager,
-		"http://127.0.0.1/signaling"
-	);
+	const string TOKEN_REFRESH_URL = "http://127.0.0.1/signaling/client_token";
+	const string SDP_EXCHANGE_API_URL = "http://127.0.0.1/signaling";
+	readonly TokenManager tokenManager;
+	readonly SdpExchangeApi sdpExchangeApi;
+	readonly RtcConnectionManager manager;
+	readonly CancellationTokenSource cts = new();
 
 	public static async Task Main(string[] args)
 	{
-		tokenManager.SetTokenRefreshUrl(TOKEN_REFRESH_URL);
+		byte[] refreshToken;
+		using (Stream? stream = typeof(Program).Assembly.GetManifestResourceStream("TR.BidsWebRtc.Console.token.txt"))
+		{
+			if (stream is null)
+			{
+				throw new FileNotFoundException("Token file not found.");
+			}
+			using var memStream = new MemoryStream();
+			await stream.CopyToAsync(memStream);
+			refreshToken = memStream.ToArray();
+		}
 
-		using var manager = RtcConnectionManager.Create(
+		using var program = new Program(refreshToken);
+		System.Console.CancelKeyPress += (_, _) =>
+		{
+			program.cts.Cancel();
+		};
+		await program.RunAsync();
+	}
+
+	private Program(byte[] refreshToken)
+	{
+		tokenManager = new(
+			httpClient,
+			refreshToken
+		);
+		if (!string.IsNullOrEmpty(TOKEN_REFRESH_URL))
+		{
+			tokenManager.SetTokenRefreshUrl(TOKEN_REFRESH_URL);
+		}
+
+		sdpExchangeApi = new(
+			httpClient,
+			tokenManager,
+			SDP_EXCHANGE_API_URL
+		);
+
+		manager = RtcConnectionManager.Create(
 			RtcConnectionManager.Role.Provider,
 			sdpExchangeApi
 		);
+		manager.OnDataGot += (sender, e) =>
+		{
+			System.Console.WriteLine($"Data got from {e.ClientId}: {Encoding.UTF8.GetString(e.Data)}");
+		};
+	}
 
+	private async Task RunAsync()
+	{
 		System.Console.WriteLine("Running...");
-		while (true)
+		BIDSSharedMemoryData bsmd = new();
+		int bsmdSize = Marshal.SizeOf<BIDSSharedMemoryData>();
+		byte[] data = new byte[bsmdSize + 4];
+		data[0] = 0x74;
+		data[1] = 0x72;
+		data[2] = 0x57;
+		data[3] = 0x42;
+		IntPtr bsmdPtr = Marshal.AllocHGlobal(bsmdSize);
+		while (!cts.IsCancellationRequested)
 		{
 			byte[] message = Encoding.UTF8.GetBytes($"Hello, World! @ {DateTime.Now}");
 			manager.BroadcastMessage(message);
 
-			await Task.Delay(1000);
+			bsmd.IsDoorClosed = !bsmd.IsDoorClosed;
+			bsmd.SpecData.C = Random.Shared.Next() % 32;
+			bsmd.StateData.Z = Random.Shared.NextDouble() * Random.Shared.Next();
+			bsmd.StateData.T = (int)DateTime.Now.TimeOfDay.TotalSeconds;
+			bsmd.StateData.V = Random.Shared.NextSingle() * Random.Shared.Next();
+
+			Marshal.StructureToPtr(bsmd, bsmdPtr, false);
+			Marshal.Copy(bsmdPtr, data, 4, bsmdSize);
+
+			manager.BroadcastMessage(data);
+
+			await Task.Delay(1000, cts.Token);
 		}
+		Marshal.FreeHGlobal(bsmdPtr);
+	}
+
+	public void Dispose()
+	{
+		cts.Cancel();
+		cts.Dispose();
+		httpClient.Dispose();
+		manager.Dispose();
 	}
 }
---
+++ TR.BidsWebRtc/Api/TokenManager.cs
@@ -0,0 +1,89 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TR.BidsWebRtc.Api;
+
+public class TokenManager(
+	HttpClient httpClient,
+	byte[] refreshToken
+	) : ITokenManager
+{
+	private readonly HttpClient _httpClient = httpClient;
+	private byte[] _refreshToken = refreshToken;
+	private string? _accessToken = null;
+	private DateTime _accessTokenExpirationTime = DateTime.MinValue;
+	public event EventHandler<byte[]>? RefreshTokenChanged;
+
+	public TokenManager(
+		HttpClient httpClient,
+		byte[] refreshToken,
+		string accessToken
+	) : this(httpClient, refreshToken)
+	{
+		SetNextAccessToken(accessToken);
+	}
+
+	private string _TokenRefreshUrl = $"https://bids-rtc.t0r.dev/signaling/client_token";
+	public void SetTokenRefreshUrl(string url) => _TokenRefreshUrl = url;
+
+	public async Task<string> GetTokenAsync()
+	{
+		if (_accessToken is not null && !IsTokenRefreshRequired())
+		{
+			return _accessToken;
+		}
+		return await RefreshTokenAsync();
+	}
+
+	private void SetNextAccessToken(string token)
+	{
+		var jwt = new JwtSecurityTokenHandler().ReadJwtToken(token);
+		var tokenExp = jwt.Claims.First(claim => claim.Type == "exp").Value;
+		var ticks = long.Parse(tokenExp);
+		_accessTokenExpirationTime = DateTimeOffset.FromUnixTimeSeconds(ticks).DateTime;
+		_accessToken = token;
+	}
+	private bool IsTokenRefreshRequired() => _accessTokenExpirationTime < DateTime.UtcNow.AddMinutes(3);
+
+	private async Task<string> RefreshTokenAsync()
+	{
+		using var request = new HttpRequestMessage(HttpMethod.Post, _TokenRefreshUrl)
+		{
+			Content = new ByteArrayContent(_refreshToken),
+			Headers = { { "Content-Type", "application/jose" } }
+		};
+		using var response = await _httpClient.SendAsync(request);
+
+		response.EnsureSuccessStatusCode();
+
+		var responseContent = await response.Content.ReadAsStringAsync();
+		string accessToken;
+		if (responseContent.StartsWith("{"))
+		{
+			using var doc = JsonDocument.Parse(responseContent);
+			var refreshToken = doc.RootElement.GetProperty("refresh_token").GetString() ?? throw new JsonException("Missing required property 'refresh_token'");
+			accessToken = doc.RootElement.GetProperty("access_token").GetString() ?? throw new JsonException("Missing required property 'access_token'");
+			byte[] nextRefreshToken = Encoding.ASCII.GetBytes(refreshToken);
+			if (!nextRefreshToken.AsSpan().SequenceEqual(_refreshToken))
+			{
+				_refreshToken = nextRefreshToken;
+				RefreshTokenChanged?.Invoke(this, nextRefreshToken);
+			}
+		}
+		else if (response.Content.Headers.ContentType?.MediaType == "application/jose")
+		{
+			accessToken = responseContent;
+		}
+		else
+		{
+			throw new InvalidOperationException($"Unexpected response content: {responseContent}");
+		}
+		SetNextAccessToken(accessToken);
+		return accessToken;
+	}
+}
